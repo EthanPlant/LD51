5bad83b0bab240ae1361cddbe4583a3d5c75040e
Ethan Plant <plant.ethan@gmail.com>
2022-10-02 17:43:17 -0400

    Last minute tileset change

---
+++ assets/tilesets/level1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="level1" tilewidth="16" tileheight="16" tilecount="104" columns="13">
+ <image source="level1.png" width="208" height="128"/>
+</tileset>
