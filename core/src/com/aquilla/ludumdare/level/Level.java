8406abbf8b535fd0b9f6dc1ea626b2a5bba9d2b2
Ethan Plant <plant.ethan@gmail.com>
2022-10-01 23:43:13 -0400

    Player checkpoints

--- core/src/com/aquilla/ludumdare/level/Level.java
+++ core/src/com/aquilla/ludumdare/level/Level.java
@@ -3,6 +3,7 @@ package com.aquilla.ludumdare.level;
 import com.aquilla.ludumdare.assets.Assets;
 import com.aquilla.ludumdare.level.entity.Player;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -18,6 +19,7 @@ public class Level {
 
     // Player
     private Player player;
+    private Vector2 lastCheckpoint;
 
     // TiledMap stuff
     private TiledMap map;
@@ -28,6 +30,7 @@ public class Level {
         renderer = new OrthogonalTiledMapRenderer(map);
 
         Vector2 spawnPoint = findPlayerSpawn();
+        lastCheckpoint = spawnPoint; // Checkpoint should be the same pos as spawn at first
         player = new Player(spawnPoint.x, spawnPoint.y, TILE_SIZE, 2 * TILE_SIZE);
         player.setAccel(new Vector2(0, -1 * GRAVITY_STRENGTH));
 
@@ -35,8 +38,8 @@ public class Level {
     }
 
     public void update(float delta) {
-        // Set player gravity
         player.update(delta, isGravityDown);
+        checkCheckpoint();
     }
 
     public void draw(SpriteBatch sb) {
@@ -48,6 +51,19 @@ public class Level {
     private Vector2 findPlayerSpawn() {
         Rectangle rect = ((RectangleMapObject) map.getLayers().get("player_spawn").getObjects().get("spawnpoint")).getRectangle();
         return new Vector2(rect.x, rect.y);
+    }
+
+    private void checkCheckpoint() {
+        // Check to see if the player is colliding with a checkpoint this frame
+        for (MapObject object : map.getLayers().get("checkpoints").getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            if (player.getBoundingBox().overlaps(rect)) { // We've hit a checkpoint, check to see if its further than the previous
+                if (rect.x > lastCheckpoint.x) {
+                    lastCheckpoint = new Vector2(rect.x, rect.y);
+                    System.out.println("Hit a new checkpoint!");
+                }
+            }
+        }
     }
 
     public void switchGravity() {
